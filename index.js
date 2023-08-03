c22727aec4089b0d4a0402abc563ab773664505e
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-08-03 16:05:35 +0300

    COmplete Scope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob"
+console.log(customerName)
+
+function upperCaseCustomerName(){
+    customerName 
+    return customerName.toUpperCase()
+}
+console.log(upperCaseCustomerName())
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+
+function overwriteBestCustomer(newName){
+    bestCustomer = newName
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "James"
+
+function changeLeastFavoriteCustomer(changeName){
+    leastFavoriteCustomer = changeName
+    return leastFavoriteCustomer
+
+}
